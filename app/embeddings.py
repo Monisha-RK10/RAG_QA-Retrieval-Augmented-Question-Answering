8b0b04f66a979e3de18c7b2e84407a32f85a8804
Monisha RK <monisha.rk2415@gmail.com>
2025-09-27 19:08:04 +0200

    Delete app/embeddings.py

--- app/embeddings.py
+++
@@ -1,9 +0,0 @@
-# Step 2: Embedding
-
-# Create Embeddings and Vector DB
-
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
-
-embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-vectordb = Chroma.from_documents(chunks, embeddings, persist_directory="db")
