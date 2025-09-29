95c55b2cb95b97b3c17244759376d125945d8116
Monisha RK <monisha.rk2415@gmail.com>
2025-09-29 18:33:19 +0200

    Update test_api.py

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -42,7 +42,7 @@ def test_pipeline():                                                            
 
 # Test FastAPI /query endpoint                                                                            # Test 2: FastAPI endpoint (end-to-end API test), mimics a real client calling the API
 def test_query_endpoint():
-    response = client.post("/query", json={"question": "What is Open-domain Question Answering?"})
+    response = client.post("/query", json={"question": "What is the advantage of Index hot-swapping?"})
     #print("API response:", response.json())
     assert response.status_code == 200
     print(response.json())
