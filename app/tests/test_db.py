fa04ebeea931add3216c43c779c7a193da04688b
Monisha RK <monisha.rk2415@gmail.com>
2025-10-07 01:19:49 +0200

    Update test_db.py

--- app/tests/test_db.py
+++ app/tests/test_db.py
@@ -6,6 +6,10 @@
   # Assert it worked.
 # So test_db.py verifies the schema + connection actually work
 
+# Note: test_db.py proves Postgres works in isolation. But in production, you want real app behavior.
+# fastapi_app.py is where we connect Postgres to the actual workflow i.e., When a user uploads a PDF → not only save file + embed → but also insert metadata row (filename, upload_time) into Postgres.
+# Without this, Postgres never gets used in the live pipeline (only in tests).
+
 from app.db_models import SessionLocal, Document
 
 def test_db_insert():
