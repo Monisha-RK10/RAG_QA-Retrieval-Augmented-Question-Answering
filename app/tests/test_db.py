b102273b404dbd36985142a118ea3f96126b8ae0
Monisha RK <monisha.rk2415@gmail.com>
2025-10-09 07:43:11 +0200

    Update test_db.py

--- app/tests/test_db.py
+++ app/tests/test_db.py
@@ -1,5 +1,5 @@
 # app/tests/test_db.py
-# Integration test for DB connectivity + schema.
+# Integration test for DB connectivity and schema, switching to SQLite for CI, (to avoid the “host not found” error).
 # Works in CI (SQLite) or locally (Postgres if available).
 
 import os
