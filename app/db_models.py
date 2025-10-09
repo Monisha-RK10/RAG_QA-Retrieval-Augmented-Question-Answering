6b700a818fb02f5475c117fdd3ed10a6832b76a8
Monisha RK <monisha.rk2415@gmail.com>
2025-10-09 13:47:54 +0200

    Update db_models.py

--- app/db_models.py
+++ app/db_models.py
@@ -1,5 +1,5 @@
 # app/db_models.py
-# Defines tables (Document), engine (create_engine(...)) and session (SessionLocal).
+# Defines tables ORM model (Document), SQLAlchemy engine (connection to DB, create_engine(...)) and SessionLocal (session factory for DB queries).
 
 from sqlalchemy import Column, Integer, String, DateTime, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
